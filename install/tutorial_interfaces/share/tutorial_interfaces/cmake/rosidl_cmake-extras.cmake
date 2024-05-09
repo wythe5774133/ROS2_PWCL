59992c99596f80d4f721e4ca464a53244e13a705
Zhong-Ding Xie <85157898+wythe5774133@users.noreply.github.com>
2024-05-10 01:12:22 +0800

    Delete install directory

--- install/tutorial_interfaces/share/tutorial_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(tutorial_interfaces_IDL_FILES "srv/DroneMissionPath.idl;srv/DroneStatus.idl")
-set(tutorial_interfaces_INTERFACE_FILES "srv/DroneMissionPath.srv;srv/DroneMissionPath_Request.msg;srv/DroneMissionPath_Response.msg;srv/DroneStatus.srv;srv/DroneStatus_Request.msg;srv/DroneStatus_Response.msg")
